f19fdd361275bce08b7e2b313edb8ef68682e9b5
Oleksii Bykovskyi <bikovskij@ukr.net>
2022-09-27 21:51:34 +0300

    add 4 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import user from '../user.json';
 import data from '../data.json';
 import friends from '../friends.json';
+import transactions from '../transactions.json';
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import { Box } from './Profile/Profile.styled';
 
 export const App = () => {
@@ -19,6 +21,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </Box>
   );
 };
